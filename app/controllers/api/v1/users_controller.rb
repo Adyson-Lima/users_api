178f0dc7c489eefe9384b55f8c911755b0590001
adyson <adyv8@yahoo.com.br>
2023-10-23 16:04:58 -0300

    15° commit - tdd controller: Consegue criar um user e retornar status 201? GREEN

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,15 @@ class Api::V1::UsersController < ApplicationController
   def show
     render json: @user
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created, location: api_v1_user_url(@user)
+    else
+      render json: @user.errors, status: :internal_server_error
+    end
+  end
 
 private
 
